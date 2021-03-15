2ed7244859a1e082ca8a59b4d34a4ca47f5691e5
yakeeliuliu <31780312+yakeeliuliu@users.noreply.github.com>
2021-03-15 12:21:00 -0400

    Create sdk_sign.js

---
+++ sdk_sign.js
@@ -0,0 +1,21 @@
+
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3({
+   endpoint: 's3-ca-central-1.amazonaws.com',   // Put you region
+   accessKeyId: 'AKIA25ZIRRWGXLOLP3EJ',       // Put you accessKeyId
+   secretAccessKey: '9X8fvFtqyXOI35DrzyncqE/xWEkLBjmKGai2ACg6',   // Put you accessKeyId
+   Bucket: 'test-yaqi',         // Put your bucket name
+   signatureVersion: 'v4',
+   region: 'ca-central-1'           // Put you region
+});
+
+
+
+const    Bucket = 'test-yaqi';
+const    Key = 'IMG_0755.JPG';
+const    Expires = 60 * 5;
+
+
+var params = {Bucket: Bucket, Key: Key};
+var authorizedUrl = s3.getSignedUrl('getObject', params);
+module.exports = authorizedUrl
